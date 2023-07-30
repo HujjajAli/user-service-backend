ff2e860273f54392bb3b5de854b4f68866274a2c
Hujaj Ali <hujaj.ali@appinsnap.com>
2023-07-30 10:26:36 +0500

    test commit from work machine

--- user-backend/src/main/java/com/product/user/UserBackendApplication.java
+++ user-backend/src/main/java/com/product/user/UserBackendApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserBackendApplication {
+	
+	//test commite from work machine
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserBackendApplication.class, args);
